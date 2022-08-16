04b54e72f2dcbf3414781eaea0adf7cf094c223b
Daniel Kariuki <freakoutbond2@gmail.com>
2022-08-16 19:34:47 +0300

    added views and templates

--- donations/payments/views.py
+++ donations/payments/views.py
@@ -1,3 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        
+    }
+    return render(request, 'home.html', context)
